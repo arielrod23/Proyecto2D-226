0d65c0b8e22a30931e04c5eb3c43ffb6fcd371cf
arielrod23 <131321996+arielrod23@users.noreply.github.com>
2026-08-21 15:46:42 -0400

    inicio
    continuar y vincular aqui

--- Assets/AnimPersonajeBasico.cs
+++ Assets/AnimPersonajeBasico.cs
@@ -1,26 +1,40 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class AnimPersonajeBasico : MonoBehaviour
 {
     [SerializeField] private float velocidad = 4f;
+    [SerializeField] private float fuerzaSalto = 6f;
+    [SerializeField] private bool saltando;
+    [SerializeField] private bool muerto;
+
+    [SerializeField] Rigidbody2D miCuerpo;
     public Animator miAnimador;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (muerto)
+            return;
+
+
         float entradaX = Input.GetAxis("Horizontal");
         Debug.Log("el jugador esta presionando " + entradaX);
-        Debug.Log("tiempo delta " + Time.deltaTime);
+        //Debug.Log("tiempo delta " + Time.deltaTime);
 
+
+        //jugador quiere moverse
         if (entradaX != 0)
         {
             transform.Translate(new Vector2(entradaX * Time.deltaTime * velocidad, 0));
+            miCuerpo.lineaVelocityX = entradaX * velocidad;
+
             miAnimador.SetBool("caminando", true);
 
             if (transform.localScale.x > 0 && entradaX < 0 || transform.localScale.x < 0 && entradaX > 0)
@@ -28,11 +42,40 @@ public class AnimPersonajeBasico : MonoBehaviour
                 GirarPersonaje();
             }
         }
-        else
+
+        private void OnGUI()
         {
-            miAnimador.SetBool("caminando", false);
+        Rect btnMuerto = new Rect(10, 10, 100, 30);
+        Rect btnDaño = new Rect(10, 0, 100, 50);
+
+        if (GUI.Button(btnPrueba, "Matar"))
+        {
+            MatarPersonaje();
         }
-    }
+        if (GUI.Button(btnDaño, "Dañar"))
+        {
+            DañarPersonaje();
+        }
+
+        void DañarPersonaje()
+        {
+            muerto = true;
+            miAnimador.SetBool("muerto", muerto);
+        }
+
+        void DañarPersonaje()
+        {
+            miAnimador.SetTrigger("daño");
+        }
+
+        //leyendo la tecla de salto
+        if (Input.GetButtonDown("Jump") && pisando)
+        {
+            miCuerpo.AddForce((fuerzaSalto), ForceMode2D.Impulse);
+        }
+
+        miAnimador.SetBool("pisando", pisando);
+        miAnimator.SetFloat("velY", miCuerpo.linearVelocityY);
 
     void GirarPersonaje()
     {
@@ -41,5 +84,26 @@ public class AnimPersonajeBasico : MonoBehaviour
                                             transform.localScale.z);
     }
 
-   
+    void MatarPersonaje()
+    {
+        muerto = true;
+        miAnimador.SetBool("muerto", muerto);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.CompareTag("piso"))
+        {
+            pisando = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("piso"))
+        {
+            pisando = false;
+        }
+    }
+
 }
